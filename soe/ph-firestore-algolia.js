dc32f63ca227163b7fcf191071dce5316f3c43e1
akeemseymens <812@holbertonschool.com>
2020-03-22 20:08:38 -0700

    added firestore upload script

--- soe/ph-firestore-algolia.js
+++ soe/ph-firestore-algolia.js
@@ -2,12 +2,17 @@
 
 require('dotenv').config();
 
+const algoliasearch = require("algoliasearch");
 const Firestore = require('@google-cloud/firestore');
 const fs = require('fs');
+const got = require('got');
 const db = new Firestore({
     projectId: process.env.GOOGLE_PROJECT_ID,
     keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
 });
+const index = algoliasearch(
+    process.env.ALGOLIA_APP_ID, process.env.ALGOLIA_SEARCH_KEY
+).initIndex(process.env.ALGOLIA_INDEX);
 
 let locations = JSON.parse(fs.readFileSync('../misc/Pit_Stops__Hand_Washing_Stations.json')).features;
 // console.log(locations[0]);
